85d0de3de4f21f545c10959f2778d53c84e02d88
Lite2GitHub <ngdiyi2@gmail.com>
2024-05-03 11:29:48 +1000

    pog

--- Project.M/Assets/Thermal/ThermalCameraScript.cs
+++ Project.M/Assets/Thermal/ThermalCameraScript.cs
@@ -1,125 +1,133 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.Assertions;
 
-public class ThermalCameraScript : MonoBehaviour {
+public class ThermalCameraScript : MonoBehaviour
+{
+    [Header("Resource References")]
 
-	[Header("Resource References")]
+    [Tooltip("ThermalVisionPostProcessing.shader")]
+    public Shader TVPostProcessing;
+    [Tooltip("ThermalVisionSurfaceReplacement.shader")]
+    public Shader TVSurfaceReplacement; // cool replacement material
+    [Tooltip("ThermalVisionPalettes.png")]
+    public Texture2D paletteTex;
 
-	[Tooltip("ThermalVisionPostProcessing.shader")]
-	public Shader TVPostProcessing;
-	[Tooltip("ThermalVisionSurfaceReplacement.shader")]
-	public Shader TVSurfaceReplacement; // cool replacement material
-	[Tooltip("ThermalVisionPalettes.png")]
-	public Texture2D paletteTex;
+    [Header("Palette")]
 
-	[Header("Palette")]
+    [Tooltip("0 - don't use texture (use 3 colors instead, as specified below)\n1 - use texture palette 1\n2 - use texture palette 2")]
+    public int useTexture = 1;
+    public Color coolColor;
+    public Color midColor;
+    public Color warmColor;
 
-	[Tooltip("0 - don't use texture (use 3 colors instead, as specified below)\n1 - use texture palette 1\n2 - use texture palette 2")]
-	public int useTexture = 1;
-	public Color coolColor;
-	public Color midColor;
-	public Color warmColor;
+    [Header("Parameters")]
+    public float environmentTemperature = 0.2f;
 
-	[Header("Parameters")]
-	public float environmentTemperature = 0.2f;
+    //--------------------------------
 
-	//--------------------------------
-	
-	bool TV = false; // Thermal Vision
-	Camera cam;
+    bool TV = true; // Thermal Vision
+    Camera cam;
 
-	Material TVPostProcessingMaterial = null;
-	
+    Material TVPostProcessingMaterial = null;
 
-	void Awake() {
-		TVPostProcessingMaterial = new Material(TVPostProcessing);
-		
 
-		cam = GetComponent<Camera>();
-	}
+    void Awake()
+    {
+        TVPostProcessingMaterial = new Material(TVPostProcessing);
 
-	void Update() {
-		List<TemperatureController> TCs = GetAllTemperatureControllers();
 
-		if (Input.GetKeyDown(KeyCode.F))
-		{
+        cam = GetComponent<Camera>();
+    }
 
-			if (TV) {
-				// replace skybox material (since replacement shade doesn't seem to affect it)
-				
+    void Update()
+    {
+        List<TemperatureController> TCs = GetAllTemperatureControllers();
 
-				// replace material tags and color for objects with explicit temperature control
-				foreach (TemperatureController TC in TCs) {
-					Renderer R = TC.gameObject.GetComponent<Renderer>();
-					if (R==null) continue;
-					TC.cachedMaterialTag = R.material.GetTag("RenderType", false);
-					TC.cachedColor = R.material.color;
-					TC.gameObject.GetComponent<Renderer>().material.SetOverrideTag("RenderType", "Temperature");
-				}
+        if (TV)
+        {
+            // replace skybox material (since replacement shade doesn't seem to affect it)
+            // replace material tags and color for objects with explicit temperature control
+            foreach (TemperatureController TC in TCs)
+            {
+                Renderer R = TC.gameObject.GetComponent<Renderer>();
+                if (R == null) continue;
+                TC.cachedMaterialTag = R.material.GetTag("RenderType", false);
+                TC.cachedColor = R.material.color;
+                TC.gameObject.GetComponent<Renderer>().material.SetOverrideTag("RenderType", "Temperature");
+            }
+            // everything else
+            cam.SetReplacementShader(TVSurfaceReplacement, "RenderType");
+        }
+        else
+        {
+            // restore skybox material
+            // if !TV
 
-				// everything else
-				cam.SetReplacementShader(TVSurfaceReplacement, "RenderType");
-				TV = false;
+            // restore temperature-controlled object tags and color
+            foreach (TemperatureController TC in TCs)
+            {
+                Renderer R = TC.gameObject.GetComponent<Renderer>();
+                if (R == null) continue;
+                TC.gameObject.GetComponent<Renderer>().material.SetOverrideTag("RenderType", TC.cachedMaterialTag);
+                TC.gameObject.GetComponent<Renderer>().material.color = TC.cachedColor;
+            }
 
-			} else {
-				// restore skybox material
-				// if !TV
+            // everything else
+            cam.ResetReplacementShader();
+            //TV = true;
+        }
 
-				// restore temperature-controlled object tags and color
-				foreach (TemperatureController TC in TCs) {
-					Renderer R = TC.gameObject.GetComponent<Renderer>();
-					if (R==null) continue;
-					TC.gameObject.GetComponent<Renderer>().material.SetOverrideTag("RenderType", TC.cachedMaterialTag);
-					TC.gameObject.GetComponent<Renderer>().material.color = TC.cachedColor;
-				}
 
-				// everything else
-				cam.ResetReplacementShader();
-				//TV = true;
-			}
-		}
+        if (TV)
+        {
+            foreach (TemperatureController TC in TCs)
+            {
+                TC.gameObject.GetComponent<Renderer>().material.color = new Color(TC.temperature, 0, 0, 0);
+            }
+        }
+    }
 
-		if (TV) {
-			foreach (TemperatureController TC in TCs) {
-				TC.gameObject.GetComponent<Renderer>().material.color = new Color(TC.temperature, 0, 0, 0);
-			}
-		}
-	}
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
 
-	void OnRenderImage(RenderTexture src, RenderTexture dst) {
+        Shader.SetGlobalFloat("_EnvironmentTemperature", environmentTemperature);
 
-		Shader.SetGlobalFloat("_EnvironmentTemperature", environmentTemperature);
+        if (TV)
+        {
+            if (useTexture == 1 || useTexture == 2)
+            {
+                TVPostProcessingMaterial.SetInt("useTexture", useTexture);
+                TVPostProcessingMaterial.SetTexture("_PaletteTex", paletteTex);
 
-		if (TV) {
-			if (useTexture==1 || useTexture==2) {
-				TVPostProcessingMaterial.SetInt("useTexture", useTexture);
-				TVPostProcessingMaterial.SetTexture("_PaletteTex", paletteTex);
+            }
+            else
+            {
+                TVPostProcessingMaterial.SetInt("useTexture", 0);
+                TVPostProcessingMaterial.SetColor("coolColor", coolColor);
+                TVPostProcessingMaterial.SetColor("midColor", midColor);
+                TVPostProcessingMaterial.SetColor("warmColor", warmColor);
+            }
+            Graphics.Blit(src, dst, TVPostProcessingMaterial);
 
-			} else {
-				TVPostProcessingMaterial.SetInt("useTexture", 0);
-				TVPostProcessingMaterial.SetColor("coolColor", coolColor);
-				TVPostProcessingMaterial.SetColor("midColor", midColor);
-				TVPostProcessingMaterial.SetColor("warmColor", warmColor);
-			}
-			Graphics.Blit(src, dst, TVPostProcessingMaterial);
+        }
+        else
+        {
+            Graphics.Blit(src, dst);
+        }
+    }
 
-		} else {
-			Graphics.Blit(src, dst);
-		}
-	}
-	
-	List<TemperatureController> GetAllTemperatureControllers() {
-		List<TemperatureController> TCs = new List<TemperatureController>();
-		foreach(TemperatureController TC in Resources.FindObjectsOfTypeAll(typeof(TemperatureController)) as TemperatureController[]) {
-			if (!EditorUtility.IsPersistent(TC.transform.root.gameObject) && 
-					!(TC.hideFlags == HideFlags.NotEditable || TC.hideFlags == HideFlags.HideAndDontSave)) {
-				TCs.Add(TC);
-			}
-		}
-		return TCs;
-	}
-
+    List<TemperatureController> GetAllTemperatureControllers()
+    {
+        List<TemperatureController> TCs = new List<TemperatureController>();
+        foreach (TemperatureController TC in Resources.FindObjectsOfTypeAll(typeof(TemperatureController)) as TemperatureController[])
+        {
+            if (!EditorUtility.IsPersistent(TC.transform.root.gameObject) &&
+                    !(TC.hideFlags == HideFlags.NotEditable || TC.hideFlags == HideFlags.HideAndDontSave))
+            {
+                TCs.Add(TC);
+            }
+        }
+        return TCs;
+    }
 }
